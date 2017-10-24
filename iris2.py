cbd0ef72c9ab4079b9acbfed6a2b8435f380decc
rvinayak1997 <33017472+rvinayak1997@users.noreply.github.com>
2017-10-24 10:48:19 +0530

    Add files via upload

---
+++ iris2.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+
+iris=load_iris()
+
+features=iris.data
+labels=iris.target
+
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(features,labels,test_size=.3)
+
+#clf=DecisionTreeClassifier()
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train,y_train)
+
+p=knn.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy=",accuracy_score(y_test,p))
+
