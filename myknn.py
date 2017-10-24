77915f2210abc850b07f8e9f3682ff78f2506100
rvinayak1997 <33017472+rvinayak1997@users.noreply.github.com>
2017-10-24 14:19:29 +0530

    Add files via upload

---
+++ myknn.py
@@ -0,0 +1,47 @@
+from scipy.spatial import distance
+
+def eucli(a,b):
+	return distance.euclidean(a,b);
+
+class myknn():
+	#fit method of classifier
+	def fit(self,x_train,y_train):
+		self.x_train=x_train
+		self.y_train=y_train
+	#predict method of classifier
+	def predict(self,x_test):
+		predictions=[]
+		for row in x_test:
+			labels=self.closest(row)
+			predictions.append(labels)
+		return predictions
+	#closest distance
+	def closest(self,row):
+		best_dist=eucli(row,self.x_train[0])
+		best_index=0
+		for i in range(1,len(self.x_train)):
+			dist=eucli(row,self.x_train[i])
+			if dist < best_dist:
+				best_dist=dist
+				best_index=i
+		return self.y_train[best_index]
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+
+iris=load_iris()
+
+features=iris.data
+labels=iris.target
+
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(features,labels,test_size=.3)
+
+clf=myknn()
+clf.fit(x_train,y_train)
+
+p=clf.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy=",accuracy_score(y_test,p))
+
